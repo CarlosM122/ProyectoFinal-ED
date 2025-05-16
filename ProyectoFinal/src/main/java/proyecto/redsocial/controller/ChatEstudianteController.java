9d7799163ebb579be6e528aa3372ba5f601ea3e7
MariaJAngulo <142365378+MariaJAngulo@users.noreply.github.com>
2025-05-16 16:17:45 -0500

    VistaEstudianteController
    Vista del chat del estudiante

---
+++ ProyectoFinal/src/main/java/proyecto/redsocial/controller/ChatEstudianteController.java
@@ -0,0 +1,51 @@
+package proyecto.redsocial.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class ChatEstudianteController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private VBox boxAyuda;
+
+    @FXML
+    private VBox boxInicio;
+
+    @FXML
+    private VBox chatBox;
+
+    @FXML
+    private VBox listCompañeros;
+
+    @FXML
+    private Label nombreCompañero;
+
+    @FXML
+    private TextField txtBuscar;
+
+    @FXML
+    private TextField txtMensaje;
+
+    @FXML
+    private Label txtinformacion;
+
+    @FXML
+    private Label txtnombre;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
+
